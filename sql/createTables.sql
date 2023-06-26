75f1fd921bbed40808a59d6c7f4efc6fba4103e6
Caio <caiob1305@gmail.com>
2023-06-26 20:51:34 -0300

    Clean Code

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -2,8 +2,8 @@ create table if not exists "users"(
 "id" SERIAL primary key,
 "name" VARCHAR(50) not null,
 "email" VARCHAR(50) not null unique,
-"password": VARCHAR(120) not null,
-"admin": BOOLEAN NOT NULL DEFAULT  FALSE 
+"password" VARCHAR(120) not null,
+"admin" BOOLEAN NOT NULL DEFAULT  FALSE 
 );
 
 create table if not exists "courses"(
@@ -15,10 +15,10 @@ create table if not exists "courses"(
 create table if not exists "usersCourses"(
 "id" SERIAL primary key,
 "active" BOOLEAN NOT NULL DEFAULT TRUE,
-"userId": INTEGER NOT NULL,
+"userId" INTEGER NOT NULL,
 FOREIGN KEY("userId")
     REFERENCES "users"(id),
-"courseId": INTEGER NOT NULL,
+"courseId" INTEGER NOT NULL,
 FOREIGN KEY("courseId")
     REFERENCES "courses"(id),
 );
