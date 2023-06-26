66b5c9420ddd3a89a0c763dca754336a27385651
Caio <caiob1305@gmail.com>
2023-06-26 01:39:14 -0300

    feat: SQL STRUCTURE

---
+++ sql/createTables.sql
@@ -0,0 +1,25 @@
+create table if not exists "users"(
+"id" SERIAL primary key,
+"name" VARCHAR(50) not null,
+"email" VARCHAR(50) not null unique,
+"password": VARCHAR(120) not null,
+"admin": BOOLEAN NOT NULL DEFAULT  FALSE 
+)
+
+create table if not exists "courses"(
+"id" SERIAL primary key,
+"name" VARCHAR(50) not null,
+"description" TEXT not null
+)
+
+create table if not exists "usersCourses"(
+"id" SERIAL primary key,
+"active" BOOLEAN NOT NULL DEFAULT TRUE,
+"userId": INTEGER NOT NULL,
+FOREIGN KEY("userId")
+    REFERENCES "users"(id),
+"courseId": INTEGER NOT NULL,
+FOREIGN KEY("courseId")
+    REFERENCES "courses"(id),
+
+)
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,3 +1,13 @@
 import { z } from "zod"
 
-const userSchema = z.object({})
+const userSchema = z.object({
+  id: z.number().positive(),
+  name: z.string().min(1).max(50),
+  email: z.string().email().min(1).max(50),
+  password: z.string().max(120).min(1),
+  admin: z.boolean().default(false)
+})
+
+const createUserSchema = userSchema.omit({
+  id: true
+})
